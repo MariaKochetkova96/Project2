7143dd65c6aa8b14df9f9073fa3baa9e897091c9
MariaKochetkova96 <khomandyakm.576@gmail.com>
2023-04-15 15:29:54 +0200

    Homework 8 task 1

---
+++ Own_calculator.py
@@ -0,0 +1,13 @@
+from calculator import simple
+
+print(simple.multip(4, 2))
+
+print(simple.sum(5, 6))
+
+print(simple.subrc(8, 3))
+
+from calculator import complex
+
+print(complex.math.sqrt(100))
+
+print(complex.math.factorial(5))
---
+++ calculator/simple.py
@@ -0,0 +1,10 @@
+def multip (a, b):
+    return a * b
+
+
+def sum(a, b):
+    return a + b
+
+
+def subrc (a, b):
+    return a - b
